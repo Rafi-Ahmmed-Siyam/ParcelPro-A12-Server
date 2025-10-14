5cf4dc2c8555c62904c937fb283c172b469c6884
Rafi-Ahmmed-Siyam <rafi.ahmmed920@gmail.com>
2025-10-15 00:56:00 +0600

    Impliment jwt and save user data API

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const express = require('express');
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const port = process.env.port || 10000;
 
-app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.wsg3r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,8 +20,39 @@ const client = new MongoClient(uri, {
       deprecationErrors: true,
    },
 });
+
+//::::::::::JWT related API and Middleware
+app.post('/jwt', async (req, res) => {
+   const { email } = req.body;
+   console.log(email);
+   const token = jwt.sign({ email: email }, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+   });
+   res.send({ token });
+});
+
 async function run() {
    try {
+      //:::::Declare all Database collection
+      const userCollection = client.db('ParcelPro').collection('users');
+      //:::::Middleware using DB
+
+      //:::::All crud operation API
+
+      //// User Related APIs ::::-------------(USER)
+      // Save user data in DB when user signup
+      app.post('/users', async (req, res) => {
+         const userData = req.body;
+         // console.log(userData);
+         const query = { email: userData.email };
+
+         const user = await userCollection.findOne(query);
+         if (user) return res.send({ message: 'User already exists' });
+
+         const result = await userCollection.insertOne(userData);
+         res.send(result);
+      });
+
       // await client.connect();
       // Send a ping to confirm a successful connection
       // await client.db('admin').command({ ping: 1 });
